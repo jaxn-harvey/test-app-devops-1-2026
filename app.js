8a0870242ff1bf73dedce7e9018d64c6c927b1cc
Harvey <jharvey2@una.edu>
2026-01-13 13:11:16 -0600

    Committed on Tuesday, Jan. 13 2026

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+
+import express from 'express'
+// es5 syntax: import express from 'express'
+// es6 syntax: import { name } from 'module-name'
+//import { express } from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express')
+})
+
+// start the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000 hurray')
+})
