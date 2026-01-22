b92858ac31ad2ba2261361211b425ba1358e13c0
Harvey <jharvey2@una.edu>
2026-01-22 13:04:27 -0600

    Jan22

--- app.mjs
+++ app.mjs
@@ -5,10 +5,20 @@ import express from 'express'
 // es6 syntax: import { name } from 'module-name'
 //import { express } from 'express'
 
-const app = express()
+//imports go up top
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
+
+const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// middlewares aka endpoints aka 'get to slash' {http verb} to slash {your endpoint}
 app.get('/', (req, res) => {
   res.send('Testing, testing.')
+  res.sendFile('index.html') // this doesn't work w/o imports, assign, arguments
+  res.sendFile(join(__dirname, 'public'));
 })
 
 // start the server
