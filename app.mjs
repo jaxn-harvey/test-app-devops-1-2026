07fb60576d791a4cd7186f122359fedc123cd9a5
Jackson Harvey <139908928+jaxn-harvey@users.noreply.github.com>
2026-01-23 12:42:05 -0600

    Adding prof's changes to app.mjs and public directory containing index.html

--- app.mjs
+++ app.mjs
@@ -1,17 +1,44 @@
-//
+//app.mjs
+//we are in ES6, use this. 
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+// import * as fs from 'node:fs';
 
-import express from 'express'
-// es5 syntax: import express from 'express'
-// es6 syntax: import { name } from 'module-name'
-//import { express } from 'express'
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+// const files = fs.readFile('.');
+// let myVar = 'demo purposes only';
 
-const app = express()
+app.use(express.json()); 
 
+// middlewares aka endpoints aka 'get to slash' {http verb} to slash {you name ur endpoint}
 app.get('/', (req, res) => {
-  res.send('Testing, testing.')
+  //res.send('Hello Express'); //string response
+  //res.sendFile('index.html'); // <- this don't work w/o imports, assign, and arguements
+  res.sendFile(join(__dirname, 'public', 'index.html')) ;
+
 })
 
-// start the server
+app.get('/api/json', (req, res) =>{
+  const myVar = 'Hello from server!';
+  res.json({ myVar });
+})
+
+app.get('/api/query', (req, res) => {
+
+  console.log("client request with query param:", req.query.name); 
+  res.json({"name": req.query.name});
+});
+
+app.post('/api/body', (req, res) => {
+  console.log("the body:", req.body); 
+  console.log("client request with body:", req.body.name); 
+  res.json({"name": req.body.name});
+});
+
+//start the server. 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000 hurray')
+  console.log('Server is running on http://localhost:3000')
 })
