cad6f566cabd119077a05f1cb3a6890472239231
Jackson Harvey <139908928+jaxn-harvey@users.noreply.github.com>
2026-02-26 13:26:35 -0600

    installed nodemon, added 'start': 'node app.mjs'

--- app.mjs
+++ app.mjs
@@ -1,3 +1,22 @@
+
+// // app.js
+
+// import express from 'express'
+// // es5 syntax: import express from 'express'
+// // es6 syntax: import { name } from 'module-name'
+// //import { express } from 'express'
+
+// const app = express()
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, Express v2')
+// })
+
+// // start the server
+// app.listen(3000, () => {
+//   console.log('Server is running on http://localhost:3000 hurray')
+// })
+
 //app.mjs
 //we are in ES6, use this. 
 import 'dotenv/config'; 
@@ -237,3 +256,4 @@ app.delete('/api/attendance/:id', async (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
 })
+
